e42d3777c7893ae5603b315573fb2f6770198a3d
gcufaro <gcufaro@itba.edu.ar>
2019-03-13 20:16:28 -0300

    Update app_main.c

--- Anemoi/main/app_main.c
+++ Anemoi/main/app_main.c
@@ -40,6 +40,7 @@ void app_main()
     enable_tdc1000_y();
     select_channel_2();
     enable_Y2_vdd();
+    //enable_X2_vdd();
 
     //Run trigger generation task, among others
     run_anemoi_tasks();
